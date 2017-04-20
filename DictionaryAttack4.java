d76d5e36fea9a1198cf7d19d324438fd0d0ed7f6
AnushreyaM <anushreyam.96@gmail.com>
2017-04-20 09:26:31 +0530

    Added comments

--- DictionaryAttack4.java
+++ DictionaryAttack4.java
@@ -4,13 +4,17 @@ class DictionaryAttack4 {
 
 	public static void main(String[] args) throws InterruptedException{
 	
-		try{
+	//populate salted and non-salted passwords 
+	try{
     		PreComputeHashes1.salted_and_unsalted();
     	}
-    	catch (Exception e){
+    	catch (FileNotFoundException e){
+		//do something
     	}
 	
+		//start timer
 		final long startTime = System.currentTimeMillis();
+		// initiliase two threads for cracking salted and non-salted passwords
 		DictionaryAttack2 d1 = new DictionaryAttack2() ;
 		DictionaryAttack3 d2 =  new DictionaryAttack3() ;
 		d1.start() ;
